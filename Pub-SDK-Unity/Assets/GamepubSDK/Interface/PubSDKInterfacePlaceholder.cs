6af3a1935db993d4d0ad908d02a4f7d17bd31b45
gamepubcorp <gamepub.system@gmail.com>
2023-04-19 16:44:12 +0900

    [update] 유니티 프로젝트 디렉토리 변경 [update] PubSDKInterfacePlaceholder 수정

---
+++ Pub-SDK-Unity/Assets/GamepubSDK/Interface/PubSDKInterfacePlaceholder.cs
@@ -0,0 +1,24 @@
+﻿//  Copyright (c) 2020-present, GamePub Corporation. All rights reserved.
+
+#if !UNITY_IOS && !UNITY_ANDROID
+namespace GamePub.PubSDK
+{
+    public class NativeInterface
+    {
+        public static void SetupSDK(string identifier, string sdkAppId) { }
+        public static void Login(string identifier, PubLoginType loginType, PubAccountServiceType serviceType) { }
+        public static void AutoLogin(string identifier) { }
+        public static void Logout(string identifier) { }
+        public static void Withdraw(string identifier) { }
+        public static void InitBilling(string identifier) { }
+        public static void Purchase(string identifier, string productId, string channelId, string characterId) { }
+        public static void RetryPurchase(string identifier, string channelId, string characterId) { }
+        public static void OpenVoided(string identifier, string channelId, string characterId) { }
+        public static void OpenTerms(string identifier) { }
+        public static void OpenImageBanner(string identifier) { }
+        public static void OpenCustomerCenter(string identifier) { }
+        public static void SetPushToken(string identifier, string pushToken) { }
+        public static void SetPushConfig(string identifier, PubPushConfig pushConfig) { }
+    }
+}
+#endif
