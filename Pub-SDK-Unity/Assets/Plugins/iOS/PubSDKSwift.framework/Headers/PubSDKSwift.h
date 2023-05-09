fb87b6c253369888801e3af97bd08ebb23b8f75b
gamepubcorp <gamepub.system@gmail.com>
2023-05-09 18:49:48 +0900

    [add] ios framework

---
+++ Pub-SDK-Unity/Assets/Plugins/iOS/PubSDKSwift.framework/Headers/PubSDKSwift.h
@@ -0,0 +1,18 @@
+//
+//  PubSDKSwift.h
+//  PubSDKSwift
+//
+//  Created by gamepub on 2022/03/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PubSDKSwift.
+FOUNDATION_EXPORT double PubSDKSwiftVersionNumber;
+
+//! Project version string for PubSDKSwift.
+FOUNDATION_EXPORT const unsigned char PubSDKSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PubSDKSwift/PublicHeader.h>
+
+
